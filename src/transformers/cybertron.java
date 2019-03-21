29bf005488e3367c28906fb751690409592a16a5
in1dm3b_15 <in1dm3b_15@HZ317743-PC.H015112.NET>
2019-03-21 13:17:00 +0100

    Un metodo que te devuelve el mismo numero introducido

--- src/transformers/cybertron.java
+++ src/transformers/cybertron.java
@@ -1,5 +1,18 @@
 package transformers;
 
-public class cybertron {
+import java.util.Scanner;
 
+public class cybertron {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int numero=0;
+		System.out.println("Introduce un numero");
+		numero=sc.nextInt();
+		System.out.println("-------------------");
+		elnumero(numero);
+	}
+	public static void elnumero(int numero) {
+		System.out.println(numero);
+	}
 }
+
